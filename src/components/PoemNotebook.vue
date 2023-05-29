d93272e7d629db5c3f9e80abe08c83b2d54bffa9
pokegh0st <me@pokeghost.org>
2023-05-29 19:58:30 +0300

    Lackluster update

--- src/components/PoemNotebook.vue
+++ src/components/PoemNotebook.vue
@@ -1,8 +1,8 @@
 <template>
     <ion-input aria-label="Name of the poem" class="top"></ion-input>
-    <div class="paper">
-        <ion-textarea aria-label="Text of the poem" @keyup="updatePoem" :auto-grow="true"
-            v-model="poemText"></ion-textarea>
+    <div class="textarea-wrapper">
+        <ion-textarea aria-label="Text of the poem" @keyup="updatePoem"
+            v-model="poemText" :auto-grow="true" class="custom"></ion-textarea>
     </div>
 </template>
 <script lang="ts">
@@ -11,7 +11,6 @@ import { Preferences } from '@capacitor/preferences';
 import { ref } from 'vue';
 
 const poem = await Preferences.get({ key: 'poem' })
-console.log(poem.value)
 
 export default {
     name: "PoemNotebook",
@@ -31,10 +30,15 @@ export default {
         getPoemValueFromStorage();
 
         const updatePoem = async (event: KeyboardEvent) => {
+            // const scrollPosition = window.scrollY;
+            // console.log(event.target)
             await Preferences.set({
                 key: 'poem',
                 value: (event.target as HTMLInputElement).value
             });
+            // (event.target as HTMLInputElement).style.height = "auto";
+            // (event.target as HTMLInputElement).style.height = ((event.target as HTMLInputElement).scrollHeight)+"px";
+            // window.scrollTo(0, scrollPosition);
         };
         return {
             updatePoem,
@@ -62,6 +66,18 @@ export default {
     width: 100%;
     height: fit-content;
     padding: 0 35px 0 35px;
+    position: relative;
+    margin-top: 0;
+    min-height: 60vh;
+}
+
+ion-textarea.custom {
+    /* border: 1px solid red!important; */
+    font-size: 22px;
+    background: repeating-linear-gradient(#F7F3EC, #F7F3EC 31px, #94ACD4 31px, #94ACD4 32px);
+    box-shadow: 0 1px 1px #00000026, 0 10px 0 -5px #eee, 0 10px 1px -4px #00000026, 0 20px 0 -10px #eee, 0 20px 1px -9px #00000026;
+    height: fit-content;
+    padding: 22px 35px 22px 35px;
     position: relative;
     margin-top: 0;
     min-height: 60vh;
